29d6352f57192e534eb7e43b3b790f63c0ab00e8
vapasi <vapasi@vapasis-MacBook-Pro.local>
2019-04-25 19:44:41 +0530

    Updated simpleloginTest with validation after login and properties tag in POM.xl

--- src/test/java/tests/SimpleLoginTest.java
+++ src/test/java/tests/SimpleLoginTest.java
@@ -3,33 +3,53 @@ package tests;
 
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
 import java.util.concurrent.TimeUnit;
+
+import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 
 public class SimpleLoginTest {
 
     ChromeDriver driver;
 
-
+    @BeforeTest
+    public void setUp() {
+        String currentUsersWorkingDir = System.getProperty("user.dir");
+        System.out.println("Dir is " + currentUsersWorkingDir);
+        System.setProperty("webdriver.chrome.driver", currentUsersWorkingDir + "/src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
 
     @Test
     public void testLogin(){
 
-
-        String currentUsersWorkingDir = System.getProperty("user.dir");
-        System.out.println("Dir is " + currentUsersWorkingDir);
-        System.setProperty("webdriver.chrome.driver",currentUsersWorkingDir+"/src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.navigate().to("https://spree-vapasi.herokuapp.com");
-        driver.navigate().to("https://spree-vapasi.herokuapp.com");
+       // driver.navigate().to("https://spree-vapasi.herokuapp.com");
         driver.findElement(By.id("link-to-login")).click();
         driver.findElement(By.id("spree_user_email")).sendKeys("spree@example.com");
         driver.findElement(By.id("spree_user_password")).sendKeys("spree123");
         driver.findElement(By.name("commit")).click();
+
+        assertTrue(driver.findElement(By.linkText("My Account")).isDisplayed());
+
+        WebElement myAccountElement = driver.findElement(By.linkText("My Account"));
+        assertEquals("My Account", myAccountElement.getText());
+
+    }
+
+
+    @AfterTest
+    public void tearDown()
+    {
         driver.close();
         driver.quit();
     }
