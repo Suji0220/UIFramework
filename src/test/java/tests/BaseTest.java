49192698b59d57dd0075469276df74329eedba5d
vapasi <vapasi@vapasis-MacBook-Pro.local>
2019-04-26 12:48:15 +0530

    Updated latest files

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class BaseTest {
+
+    ChromeDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        String currentUsersWorkingDir = System.getProperty("user.dir");
+        System.out.println("Dir is " + currentUsersWorkingDir);
+        System.setProperty("webdriver.chrome.driver", currentUsersWorkingDir + "/src/test/resources/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+    @AfterMethod
+    public void tearDown()
+    {
+        driver.close();
+        driver.quit();
+    }
+
+}
--- src/test/java/tests/SimpleLoginTest.java
+++ src/test/java/tests/SimpleLoginTest.java
@@ -1,35 +1,14 @@
 package tests;
 
-
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
-
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
 
-public class SimpleLoginTest {
+public class SimpleLoginTest extends BaseTest{
 
-    ChromeDriver driver;
-
-    @BeforeTest
-    public void setUp() {
-        String currentUsersWorkingDir = System.getProperty("user.dir");
-        System.out.println("Dir is " + currentUsersWorkingDir);
-        System.setProperty("webdriver.chrome.driver", currentUsersWorkingDir + "/src/test/resources/chromedriver");
-        driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-    }
-
-    @Test
+  // @Test
     public void testLogin(){
 
         driver.navigate().to("https://spree-vapasi.herokuapp.com");
@@ -46,13 +25,6 @@ public class SimpleLoginTest {
 
     }
 
-
-    @AfterTest
-    public void tearDown()
-    {
-        driver.close();
-        driver.quit();
-    }
 
 
 
